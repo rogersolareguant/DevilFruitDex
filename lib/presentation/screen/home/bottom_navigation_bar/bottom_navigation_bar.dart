864c835903215d3f8480749e58c3215ad589c6b7
Roger Sola <roger.sola-reguant.external@capgemini.com>
2025-01-31 11:33:32 +0100

    BottomNavBar correct 1.0

--- lib/presentation/screen/home/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/presentation/screen/home/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -1,29 +1,22 @@
 import 'package:devilfruitdex/presentation/screen/home/bottom_navigation_bar/views/map/map_view.dart';
-import 'package:devilfruitdex/presentation/screen/home/cubit/devil_fruit_cubit.dart';
 import 'package:devilfruitdex/presentation/screen/home/bottom_navigation_bar/views/settings/settings_view.dart';
 import 'package:devilfruitdex/presentation/screen/home/bottom_navigation_bar/views/favourite_fruits/fav_view.dart';
 import 'package:devilfruitdex/presentation/screen/home/bottom_navigation_bar/views/all_fruits/home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_lazy_indexed_stack/flutter_lazy_indexed_stack.dart';
 
 class BottomNavigationBarScreen extends StatefulWidget {
-  
   const BottomNavigationBarScreen({
     super.key,
   });
 
   @override
-  State<BottomNavigationBarScreen> createState() => _BottomNavigationBarScreenState();
+  State<BottomNavigationBarScreen> createState() =>
+      _BottomNavigationBarScreenState();
 }
 
 class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
-  final List<Widget Function()> _widgetOptions = [
-    () => MapView(),
-    () => const FavView(),
-    () => const HomeView(),
-    () => const SettingsView(),
-  ];
+
   int _selectedIndex = 2;
 
   void _onItemTap(int index) {
@@ -34,66 +27,67 @@ class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DevilFruitCubit, DevilFruitState>(
-      builder: (context, state) {
-        return Scaffold(
-          body: LazyIndexedStack(
-            index: _selectedIndex,
-            children: _widgetOptions.map((builder) => builder()).toList(),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.shifting,
-              currentIndex: _selectedIndex,
-              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              onTap: _onItemTap,
-              items: [
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.map_outlined,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    activeIcon: Icon(
-                      Icons.map,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    label: '',
-                    backgroundColor: Theme.of(context).scaffoldBackgroundColor),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.favorite_outline,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    activeIcon: Icon(
-                      Icons.favorite,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    label: '',
-                    backgroundColor: Theme.of(context).scaffoldBackgroundColor),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.home_outlined,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    activeIcon: Icon(
-                      Icons.home,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    label: '',
-                    backgroundColor: Theme.of(context).scaffoldBackgroundColor),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.settings_outlined,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    activeIcon: Icon(
-                      Icons.settings,
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
-                    label: '',
-                    backgroundColor: Theme.of(context).scaffoldBackgroundColor),
-              ]),
-        );
-      },
+    return Scaffold(
+      body: LazyIndexedStack(
+        index: _selectedIndex,
+        children: [
+          MapView(),
+          FavView(),
+          HomeView(),
+          SettingsView()
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          onTap: _onItemTap,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.map_outlined,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                activeIcon: Icon(
+                  Icons.map,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                label: '',
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite_outline,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                activeIcon: Icon(
+                  Icons.favorite,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                label: '',
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                activeIcon: Icon(
+                  Icons.home,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                label: '',
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.settings_outlined,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                activeIcon: Icon(
+                  Icons.settings,
+                  color: Theme.of(context).primaryIconTheme.color,
+                ),
+                label: '',
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor),
+          ]),
     );
   }
 }
